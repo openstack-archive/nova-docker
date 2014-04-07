e5b1147913a575c8cd28e26c23bfaf00accebf33
Aaron Rosen <aaronorosen@gmail.com>
2014-04-07 12:51:29 -0700

    Implement DockerGenericVIFDriver
    This patch implements a generic vif driver which will one to use various
    different vif drivers inconjunction with docker.
    
    Change-Id: Ib306abb6e43430afe7a2a4f8086088320d3671af

---
+++ novadocker/virt/docker/vifs.py
@@ -0,0 +1,84 @@
+# Copyright (C) 2013 VMware, Inc
+# Copyright 2011 OpenStack Foundation
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+
+from nova import exception
+from nova.network import linux_net
+from nova.network import model as network_model
+from nova.openstack.common.gettextutils import _
+from nova.openstack.common import log as logging
+from nova import utils
+from novadocker.virt.docker import network
+
+LOG = logging.getLogger(__name__)
+
+
+class DockerGenericVIFDriver(object):
+
+    def plug(self, instance, vif, container_id):
+        vif_type = vif['type']
+
+        LOG.debug(_('vif_type=%(vif_type)s instance=%(instance)s '
+                    'vif=%(vif)s'),
+                  {'vif_type': vif_type, 'instance': instance,
+                   'vif': vif})
+
+        if vif_type is None:
+            raise exception.NovaException(
+                _("vif_type parameter must be present "
+                  "for this vif_driver implementation"))
+
+        if vif_type == network_model.VIF_TYPE_BRIDGE:
+            self.plug_bridge(instance, vif, container_id)
+        else:
+            raise exception.NovaException(
+                _("Unexpected vif_type=%s") % vif_type)
+
+    def plug_bridge(self, instance, vif, container_id):
+        if_local_name = 'tap%s' % vif['id'][:11]
+        if_remote_name = 'ns%s' % vif['id'][:11]
+        bridge = vif['network']['bridge']
+        gateway = network.find_gateway(instance, vif['network'])
+        ip = network.find_fixed_ip(instance, vif['network'])
+
+        # Device already exists so return.
+        if linux_net.device_exists(if_local_name):
+            return
+        undo_mgr = utils.UndoManager()
+
+        try:
+            utils.execute('ip', 'link', 'add', 'name', if_local_name, 'type',
+                          'veth', 'peer', 'name', if_remote_name,
+                          run_as_root=True)
+            undo_mgr.undo_with(lambda: utils.execute(
+                'ip', 'link', 'delete', if_local_name, run_as_root=True))
+            # NOTE(samalba): Deleting the interface will delete all
+            # associated resources (remove from the bridge, its pair, etc...)
+            utils.execute('brctl', 'addif', bridge, if_local_name,
+                          run_as_root=True)
+            utils.execute('ip', 'link', 'set', if_local_name, 'up',
+                          run_as_root=True)
+            utils.execute('ip', 'link', 'set', if_remote_name, 'netns',
+                          container_id, run_as_root=True)
+            utils.execute('ip', 'netns', 'exec', container_id, 'ifconfig',
+                          if_remote_name, ip, run_as_root=True)
+            utils.execute('ip', 'netns', 'exec', container_id,
+                          'ip', 'route', 'replace', 'default', 'via',
+                          gateway, 'dev', if_remote_name, run_as_root=True)
+        except Exception:
+            LOG.exception("Failed to configure network")
+            msg = _('Failed to setup the network, rolling back')
+            undo_mgr.rollback_and_reraise(msg=msg, instance=instance)
