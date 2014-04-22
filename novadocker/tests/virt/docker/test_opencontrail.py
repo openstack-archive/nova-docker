4c41e4a8aa2dcb8bfb309af383b9c80fc0323d5c
Ted Ghose <rintu@hotmail.com>
2014-04-22 15:44:55 -0700

    OpenContrail support for nova-docker
    Following commits are bundeled:
    
    Make the vif_driver configurable -Pedro Marques
    
    Initial support for OpenContrail -Pedro Marques
    
    Revert "Make the vif_driver configurable" -Martin Loschwitz
    
    This reverts commit 4460711abd3e87317e4c5b24eee26aa96cdd8990.
    
    Split "plug" function into a "plug" and an "attach" function -Martin
    Loschwitz
    
    Recent versions of docker expect two functions, one named "plug" and the
    other named "attach". This commit changes the code so that both functions
    exist and do the right thing to make Docker happy. Without the patch, the
    correct association of interfaces and network namespaces is broken (i.e.
    the driver will try to add an interface to a namespace that does nut exist
    yet).
    
    Adapt this driver to the new i18n framework in Nova -Martin Loschwitz
    
    Adapt to latest changes in OpenContrail -Martin Loschwitz
    
    Don't make the driver crash nova-compute if veth if already exists
    -Martin Loschwitz
    
    In its current form, the opencontrail driver for docker will try to create
    a veth interface even if it's already there. This will result in an error
    that will make nova-compute crash. In fact, nova-compute is unrestartable
    with the current driver. This patch adds a check to see if the veth if is
    already there and does not try to add it a second time if it already is.
    
    removed thrift call, using ContrailVRouterApi -Ted Ghose
    
    Added tests -Ted Ghose
    
    Change-Id: I1d032143a4c346d3aa7169017301201c278098d3

---
+++ novadocker/tests/virt/docker/test_opencontrail.py
@@ -0,0 +1,157 @@
+# Copyright (C) 2014 Juniper Networks, Inc
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+import mock
+import sys
+
+
+class MockContrailVRouterApi(mock.MagicMock):
+    def __init__(self, *a, **kw):
+        super(MockContrailVRouterApi, self).__init__()
+        self._args = a
+        self._dict = kw
+        self._vifs = {}
+
+    def add_port(self, vm_uuid_str, vif_uuid_str, interface_name,
+                 mac_address, **kwargs):
+        if vm_uuid_str not in self._vifs:
+            self._vifs[vif_uuid_str] = dict(vm=vm_uuid_str,
+                                            intf=interface_name,
+                                            mac=mac_address)
+            self._vifs[vm_uuid_str].update(kwargs)
+            return self._vifs[vif_uuid_str]
+
+    def delete_port(self, vif_uuid_str):
+        if vif_uuid_str in self._vifs:
+            del self._vifs[vif_uuid_str]
+
+
+mock_pkg = mock.MagicMock(name='mock_contrail_vrouter_api')
+mock_mod = mock.MagicMock(name='mock_vrouter_api')
+mock_cls = mock.MagicMock(name='MockOpenContrailVIFDriver',
+                          side_effect=MockContrailVRouterApi)
+mock_mod.OpenContrailVIFDriver = mock_cls
+mock_pkg.vrouter_api = mock_mod
+
+sys.modules['contrail_vrouter_api'] = mock_pkg
+sys.modules['contrail_vrouter_api.vrouter_api'] = mock_mod
+
+from nova.network import model as network_model
+from nova import test
+from novadocker.virt.docker import driver as docker_driver
+
+
+class DockerOpenContrailVIFDriverTestCase(test.TestCase):
+    def setUp(self):
+        super(DockerOpenContrailVIFDriverTestCase, self).setUp()
+        docker_driver.CONF.set_override(
+            'vif_driver',
+            'novadocker.virt.docker.opencontrail.OpenContrailVIFDriver',
+            group='docker')
+
+    def test_plug_vrouter(self):
+        vid = '920be1f4-2b98-411e-890a-69bcabb2a5a0'
+        address = '10.1.2.1'
+        calls = [
+            mock.call('ip', 'link', 'add', 'veth%s' % vid[:8],
+                      'type', 'veth', 'peer', 'name',
+                      'ns%s' % vid[:8], run_as_root=True),
+            mock.call('ip', 'link', 'set', 'ns%s' % vid[:8],
+                      'address', address, run_as_root=True),
+        ]
+        network_info = [network_model.VIF(id=vid, address=address)]
+        with mock.patch('nova.utils.execute') as ex:
+            driver = docker_driver.DockerDriver(object)
+            driver.plug_vifs({'name': 'fake_instance'}, network_info)
+            ex.assert_has_calls(calls)
+
+    def test_plug_two_vrouters(self):
+        vid1 = '921be2f4-2b98-411e-890a-69bcabb2a5a1'
+        address1 = '10.1.2.2'
+        vid2 = '922be3f4-2b98-411e-890a-69bcabb2a5a2'
+        address2 = '10.1.2.3'
+        calls = [
+            mock.call('ip', 'link', 'add', 'veth%s' % vid1[:8],
+                      'type', 'veth', 'peer', 'name',
+                      'ns%s' % vid1[:8], run_as_root=True),
+            mock.call('ip', 'link', 'set', 'ns%s' % vid1[:8],
+                      'address', address1, run_as_root=True),
+            mock.call('ip', 'link', 'add', 'veth%s' % vid2[:8],
+                      'type', 'veth', 'peer', 'name',
+                      'ns%s' % vid2[:8], run_as_root=True),
+            mock.call('ip', 'link', 'set', 'ns%s' % vid2[:8],
+                      'address', address2, run_as_root=True),
+        ]
+        network_info = [network_model.VIF(id=vid1, address=address1),
+                        network_model.VIF(id=vid2, address=address2)]
+        with mock.patch('nova.utils.execute') as ex:
+            driver = docker_driver.DockerDriver(object)
+            driver.plug_vifs({'name': 'fake_instance'}, network_info)
+            ex.assert_has_calls(calls)
+
+    @mock.patch.object(docker_driver.DockerDriver,
+                       '_find_container_by_name',
+                       return_value={'id': 'my_vm'})
+    @mock.patch.object(docker_driver.DockerDriver,
+                       '_find_container_pid',
+                       return_value=7890)
+    def test_attach_vrouter(self, mock_find_by_name, mock_find_pid):
+        vid = '920be1f5-2b98-411e-890a-69bcabb2a5a0'
+        address = '10.1.2.1'
+        calls = [
+            mock.call('mkdir', '-p', '/var/run/netns', run_as_root=True),
+            mock.call('ln', '-sf', '/proc/7890/ns/net',
+                      '/var/run/netns/my_vm', run_as_root=True),
+            mock.call('ip', 'link', 'set', 'ns%s' % vid[:8], 'netns', 'my_vm',
+                      run_as_root=True),
+            mock.call('ip', 'link', 'set', 'veth%s' % vid[:8], 'up',
+                      run_as_root=True),
+            mock.call('ip', 'netns', 'exec', 'my_vm', 'dhclient',
+                      'ns%s' % vid[:8], run_as_root=True),
+        ]
+        network_info = [network_model.VIF(id=vid, address=address,
+                                          network=network_model.Network(
+                                              id='virtual-network-1',
+                                              subnets=[network_model.Subnet(
+                                                  cidr='1.1.1.0/24',
+                                                  gateway=network_model.IP(
+                                                      address='1.1.1.254',
+                                                      type='gateway'),
+                                                  ips=[network_model.IP(
+                                                      address='1.1.1.42',
+                                                      type='fixed',
+                                                      version=4)]
+                                              )]
+                                          ))]
+        instance = dict(name='fake_instance', display_name='fake_vm',
+                        hostname='fake_vm', host='linux',
+                        project_id='e2d2ddc6-4e0f-4cd4-b846-3bad53093ec6',
+                        uuid='d4b817fb-7885-4649-bad7-89302dde12e1')
+        with mock.patch('nova.utils.execute') as ex:
+            driver = docker_driver.DockerDriver(object)
+            driver._attach_vifs(instance, network_info)
+            ex.assert_has_calls(calls)
+
+    def test_unplug_vrouter(self):
+        vid = '920be1f4-2b98-411e-890a-69bcabb2a5a0'
+        address = '10.1.2.1'
+        calls = [
+            mock.call('ip', 'link', 'delete', 'veth%s' % vid[:8],
+                      run_as_root=True),
+        ]
+        network_info = [network_model.VIF(id=vid, address=address)]
+        with mock.patch('nova.utils.execute') as ex:
+            driver = docker_driver.DockerDriver(object)
+            driver.unplug_vifs({'name': 'fake_instance'}, network_info)
+            ex.assert_has_calls(calls)
---
+++ novadocker/virt/docker/opencontrail.py
@@ -0,0 +1,108 @@
+# Copyright (C) 2014 Juniper Networks, Inc
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+from contrail_vrouter_api.vrouter_api import ContrailVRouterApi
+from nova.i18n import _
+from nova.network import linux_net
+from nova import utils
+from oslo_log import log as logging
+
+LOG = logging.getLogger(__name__)
+
+
+class OpenContrailVIFDriver(object):
+    def __init__(self):
+        self._vrouter_client = ContrailVRouterApi(doconnect=True)
+
+    def plug(self, instance, vif):
+        if_local_name = 'veth%s' % vif['id'][:8]
+        if_remote_name = 'ns%s' % vif['id'][:8]
+
+        # Device already exists so return.
+        if linux_net.device_exists(if_local_name):
+            return
+        undo_mgr = utils.UndoManager()
+
+        try:
+            utils.execute('ip', 'link', 'add', if_local_name, 'type', 'veth',
+                          'peer', 'name', if_remote_name, run_as_root=True)
+            undo_mgr.undo_with(lambda: utils.execute(
+                'ip', 'link', 'delete', if_local_name, run_as_root=True))
+
+            utils.execute('ip', 'link', 'set', if_remote_name, 'address',
+                          vif['address'], run_as_root=True)
+
+        except Exception:
+            LOG.exception("Failed to configure network")
+            msg = _('Failed to setup the network, rolling back')
+            undo_mgr.rollback_and_reraise(msg=msg, instance=instance)
+
+    def attach(self, instance, vif, container_id):
+        if_local_name = 'veth%s' % vif['id'][:8]
+        if_remote_name = 'ns%s' % vif['id'][:8]
+
+        undo_mgr = utils.UndoManager()
+        ipv4_address = '0.0.0.0'
+        ipv6_address = None
+        if 'subnets' in vif['network']:
+            subnets = vif['network']['subnets']
+            for subnet in subnets:
+                ips = subnet['ips'][0]
+                if (ips['version'] == 4):
+                    if ips['address'] is not None:
+                        ipv4_address = ips['address']
+                if (ips['version'] == 6):
+                    if ips['address'] is not None:
+                        ipv6_address = ips['address']
+        params = {
+            'ip_address': ipv4_address,
+            'vn_id': vif['network']['id'],
+            'display_name': instance['display_name'],
+            'hostname': instance['hostname'],
+            'host': instance['host'],
+            'vm_project_id': instance['project_id'],
+            'port_type': 'NovaVMPort',
+            'ip6_address': ipv6_address,
+        }
+
+        try:
+            utils.execute('ip', 'link', 'set', if_remote_name, 'netns',
+                          container_id, run_as_root=True)
+
+            result = self._vrouter_client.add_port(
+                instance['uuid'], vif['id'],
+                if_local_name, vif['address'], **params)
+            if not result:
+                # follow the exception path
+                raise RuntimeError('add_port returned %s' % str(result))
+            utils.execute('ip', 'link', 'set', if_local_name, 'up',
+                          run_as_root=True)
+        except Exception:
+            LOG.exception("Failed to attach the network")
+            msg = _('Failed to attach the network, rolling back')
+            undo_mgr.rollback_and_reraise(msg=msg, instance=instance)
+
+        # TODO(NetNS): attempt DHCP client; fallback to manual config if the
+        # container doesn't have an working dhcpclient
+        utils.execute('ip', 'netns', 'exec', container_id, 'dhclient',
+                      if_remote_name, run_as_root=True)
+
+    def unplug(self, instance, vif):
+        try:
+            self._vrouter_client.delete_port(vif['id'])
+        except Exception:
+            LOG.exception(_("Delete port failed"), instance=instance)
+
+        if_local_name = 'veth%s' % vif['id'][:8]
+        utils.execute('ip', 'link', 'delete', if_local_name, run_as_root=True)
