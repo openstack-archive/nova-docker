53c0854dde375872e79cd80bc3d56a51abe737ac
Eric Windisch <ewindisch@docker.com>
2014-06-26 14:12:59 -0400

    Load and save images in glance directly
    Removes dependency on private docker-registry.
    
    Co-Authored-By: Ian Main <imain@redhat.com>
    Co-Authored-By: Chris Alfonso <calfonso@redhat.com>
    
    Change-Id: Ib14a1f828af3b1e932caa3d314fe55d83f53534f

--- contrib/devstack/prepare_devstack.sh
+++ contrib/devstack/prepare_devstack.sh
@@ -6,19 +6,11 @@ env
 NOVADOCKERDIR=$(realpath $(dirname $0)/../..)
 INSTALLDIR=${INSTALLDIR:-/opt/stack}
 
-# Allow the registry container to access keystone and glance
-sudo iptables -I INPUT -p tcp -m tcp --src 172.17.0.0/16 --dport 35357 -j ACCEPT
-sudo iptables -I INPUT -p tcp -m tcp --src 172.17.0.0/16 --dport 9292 -j ACCEPT
-
 cp $NOVADOCKERDIR/contrib/devstack/extras.d/70-docker.sh $INSTALLDIR/devstack/extras.d/
 cp $NOVADOCKERDIR/contrib/devstack/lib/nova_plugins/hypervisor-docker $INSTALLDIR/devstack/lib/nova_plugins/
 
-HOST_IP=$(ip addr | grep -Eo "inet [0-9\.]+" | grep -v 127.0.0.1 | head -n 1 | cut -d " " -f 2)
 cat - <<-EOF >> $INSTALLDIR/devstack/localrc
 export VIRT_DRIVER=docker
-export HOST_IP=$HOST_IP
-export KEYSTONE_ADMIN_BIND_HOST=0.0.0.0
-export DOCKER_REGISTRY_IMAGE=registry
 export DEFAULT_IMAGE_NAME=cirros
 export IMAGE_URLS=" "
 EOF
