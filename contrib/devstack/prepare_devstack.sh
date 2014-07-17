010e0c0d5ab807b8b110f3ecead7953af0cefd0a
Chris Alfonso <calfonso@redhat.com>
2014-07-17 11:49:04 -0600

    Add tempest configuration and add it to the check job.
    This will copy the docker tempest configuration file into the devstack tempest
    directory. This patch also turns on the full tempest run, which should use our
    tempest config.
    
    Change-Id: I489e582e39b5de865999f14b026ef2305be367af

--- contrib/devstack/prepare_devstack.sh
+++ contrib/devstack/prepare_devstack.sh
@@ -12,6 +12,7 @@ sudo iptables -I INPUT -p tcp -m tcp --src 172.17.0.0/16 --dport 9292 -j ACCEPT
 
 cp $NOVADOCKERDIR/contrib/devstack/extras.d/70-docker.sh $INSTALLDIR/devstack/extras.d/
 cp $NOVADOCKERDIR/contrib/devstack/lib/nova_plugins/hypervisor-docker $INSTALLDIR/devstack/lib/nova_plugins/
+cp $NOVADOCKERDIR/contrib/devstack/tempest/etc/tempest.conf $INSTALLDIR/tempest/etc/
 
 HOST_IP=$(ip addr | grep -Eo "inet [0-9\.]+" | grep -v 127.0.0.1 | head -n 1 | cut -d " " -f 2)
 cat - <<-EOF >> $INSTALLDIR/devstack/localrc
