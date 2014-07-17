cd9e4238957554d420b8b045b92c841bf45481bd
Chris Alfonso <calfonso@redhat.com>
2014-07-17 11:49:04 -0600

    Turns off a subset of tempest tests to enable a valid tempest run
    The prepare_devstack.sh will just append the setting to the end of the tempest.conf
    
    Change-Id: I489e582e39b5de865999f14b026ef2305be367af

--- contrib/devstack/prepare_devstack.sh
+++ contrib/devstack/prepare_devstack.sh
@@ -13,6 +13,17 @@ sudo iptables -I INPUT -p tcp -m tcp --src 172.17.0.0/16 --dport 9292 -j ACCEPT
 cp $NOVADOCKERDIR/contrib/devstack/extras.d/70-docker.sh $INSTALLDIR/devstack/extras.d/
 cp $NOVADOCKERDIR/contrib/devstack/lib/nova_plugins/hypervisor-docker $INSTALLDIR/devstack/lib/nova_plugins/
 
+locate tempest.conf
+# Turn off tempest test suites
+cat - <<-EOF >> $INSTALLDIR/devstack/tempest/etc/tempest.conf
+# The following settings have been turned off for nova-docker
+swift=False
+ceilometer=False
+resize=False
+suspend=False
+rescue=False
+EOF
+
 HOST_IP=$(ip addr | grep -Eo "inet [0-9\.]+" | grep -v 127.0.0.1 | head -n 1 | cut -d " " -f 2)
 cat - <<-EOF >> $INSTALLDIR/devstack/localrc
 export VIRT_DRIVER=docker
