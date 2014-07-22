95ed3a8224a6be6349aa0f7099cc624e89667862
Ian Main <imain@redhat.com>
2014-07-22 15:53:34 -0400

    WIP: Test tempest configuration
    Attempting to get a full tempest test to run properly.
    Now on Ubuntu.
    
    Change-Id: Ia6983d03a67d2ccf268040d9410456f4f2179b76

--- novadocker/virt/docker/vifs.py
+++ novadocker/virt/docker/vifs.py
@@ -14,6 +14,7 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+from oslo.config import cfg
 
 from nova import exception
 from nova.network import linux_net
@@ -25,6 +26,18 @@ from nova import utils
 from novadocker.virt.docker import network
 
 LOG = logging.getLogger(__name__)
+
+libvirt_vif_opts = [
+    cfg.BoolOpt('use_virtio_for_bridges',
+                default=True,
+                help='Use virtio for bridge interfaces with KVM/QEMU'),
+]
+
+
+CONF = cfg.CONF
+CONF.register_opts(libvirt_vif_opts, 'libvirt')
+CONF.import_opt('virt_type', 'nova.virt.libvirt.driver', group='libvirt')
+CONF.import_opt('use_ipv6', 'nova.netconf')
 
 
 class DockerGenericVIFDriver(object):
@@ -92,6 +105,29 @@ class DockerGenericVIFDriver(object):
         if_local_name = 'tap%s' % vif['id'][:11]
         if_remote_name = 'ns%s' % vif['id'][:11]
         bridge = vif['network']['bridge']
+
+        if (not network.get_meta('multi_host', False) and
+            network.get_meta('should_create_bridge', False)):
+            if network.get_meta('should_create_vlan', False):
+                iface = CONF.vlan_interface or \
+                        network.get_meta('bridge_interface')
+                LOG.debug('Ensuring vlan %(vlan)s and bridge %(bridge)s',
+                          {'vlan': network.get_meta('vlan'),
+                           'bridge': self.get_bridge_name(vif)},
+                          instance=instance)
+                linux_net.LinuxBridgeInterfaceDriver.ensure_vlan_bridge(
+                                             network.get_meta('vlan'),
+                                             self.get_bridge_name(vif),
+                                             iface)
+            else:
+                iface = CONF.flat_interface or \
+                            network.get_meta('bridge_interface')
+                LOG.debug("Ensuring bridge %s",
+                          self.get_bridge_name(vif), instance=instance)
+                linux_net.LinuxBridgeInterfaceDriver.ensure_bridge(
+                                        self.get_bridge_name(vif),
+                                        iface)
+
         gateway = network.find_gateway(instance, vif['network'])
         ip = network.find_fixed_ip(instance, vif['network'])
 
