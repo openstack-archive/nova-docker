openstack-archive/nova-docker

commit 9fa622780f708644778bbe62d11991b30892833c

Fix unicode handling for image names.
This fixes one of the unit tests which expects to be able to use
unicode instance names.

Co-Authored-By: Ian Main <imain@redhat.com>
Change-Id: Iaa944ac5af25ee5c5e44d2beb80c5019c812bfb9

novadocker/virt/docker/client.py
@@ -15,6 +15,7 @@
 
 import functools
 import socket
+import urllib
 
 from eventlet.green import httplib
 import six
@@ -111,8 +112,9 @@ class DockerHTTPClient(object):
             headers['Content-Type'] = 'application/json'
             kwargs['headers'] = headers
         conn = self.connection
-        conn.request(*args, **kwargs)
-        return Response(conn.getresponse(), url=args[1])
+        encoded_args = args[0], urllib.quote(args[1])
+        conn.request(*encoded_args, **kwargs)
+        return Response(conn.getresponse(), url=encoded_args[1])
 
     def list_containers(self, _all=True):
         resp = self.make_request(
@@ -143,7 +145,8 @@ class DockerHTTPClient(object):
         data.update(args)
         resp = self.make_request(
             'POST',
-            '/v1.7/containers/create?name={0}'.format(name),
+            '/v1.7/containers/create?name={0}'.format(str(name).
+                                                      encode('utf-8')),
             body=jsonutils.dumps(data))
         if resp.code != 201:
             return
@@ -176,7 +179,7 @@ class DockerHTTPClient(object):
     def inspect_image(self, image_name):
         resp = self.make_request(
             'GET',
-            '/v1.7/images/{0}/json'.format(image_name))
+            '/v1.7/images/{0}/json'.format(str(image_name).encode('utf-8')))
         if resp.code != 200:
             return
         return resp.to_json()
@@ -209,7 +212,7 @@ class DockerHTTPClient(object):
         return (resp.code == 204)
 
     def get_image(self, name, size=4096):
-        parts = name.rsplit(':', 1)
+        parts = str(name).encode('utf-8').rsplit(':', 1)
         url = '/v1.13/images/{0}/get'.format(parts[0])
         resp = self.make_request('GET', url)
 
@@ -221,7 +224,7 @@ class DockerHTTPClient(object):
         return
 
     def get_image_resp(self, name):
-        parts = name.rsplit(':', 1)
+        parts = str(name).encode('utf-8').rsplit(':', 1)
         url = '/v1.13/images/{0}/get'.format(parts[0])
         resp = self.make_request('GET', url)
         return resp
@@ -232,10 +235,10 @@ class DockerHTTPClient(object):
 
     def load_repository_file(self, name, path):
         with open(path) as fh:
-            self.load_repository(name, fh)
+            self.load_repository(str(name).encode('utf-8'), fh)
 
     def commit_container(self, container_id, name):
-        parts = name.rsplit(':', 1)
+        parts = str(name).encode('utf-8').rsplit(':', 1)
         url = '/v1.7/commit?container={0}&repo={1}'.format(container_id,
                                                            parts[0])
         if len(parts) > 1:

commit 786fe3a3caad630a25746bb98b8e8ab97d167d4a

Fix urlencoding of Docker API requests
Docker API requests were broken by urlencoding introduced in commit:
 9fa622780f708644778bbe62d11991b30892833c

The problem with this patch was that a simple urllib.quote()
was performed against the entire URL, although this would
always escape the following characters which were only conditionally
supposed to be quoted: ? & =

By parameterizing the requests and using urllib.urlencode(), we
can apply the quoting only to the components of the URL which
require percent-encoding.

Change-Id: I8c5aa26ec96e3bc7b0233aa2aaa6b15afd26bf9e

novadocker/virt/docker/client.py
@@ -112,7 +112,12 @@ class DockerHTTPClient(object):
             headers['Content-Type'] = 'application/json'
             kwargs['headers'] = headers
         conn = self.connection
-        encoded_args = args[0], urllib.quote(args[1])
+
+        url = args[1]
+        if len(args) > 2:
+            url += urllib.urlencode(args[2:])
+        encoded_args = args[0], url
+
         conn.request(*encoded_args, **kwargs)
         return Response(conn.getresponse(), url=encoded_args[1])
 
@@ -147,8 +152,8 @@ class DockerHTTPClient(object):
         data.update(args)
         resp = self.make_request(
             'POST',
-            '/v1.7/containers/create?name={0}'.format(str(name).
-                                                      encode('utf-8')),
+            '/v1.7/containers/create?',
+            ('name', str(name).encode('utf-8')),
             body=jsonutils.dumps(data))
         if resp.code != 201:
             return
@@ -181,7 +186,8 @@ class DockerHTTPClient(object):
     def inspect_image(self, image_name):
         resp = self.make_request(
             'GET',
-            '/v1.7/images/{0}/json'.format(str(image_name).encode('utf-8')))
+            '/v1.7/images/{0}/json'.format(
+            urllib.quote(str(image_name).encode('utf-8'))))
         if resp.code != 200:
             return
         return resp.to_json()
@@ -198,7 +204,8 @@ class DockerHTTPClient(object):
         timeout = 5
         resp = self.make_request(
             'POST',
-            '/v1.7/containers/{0}/stop?t={1}'.format(container_id, timeout))
+            '/v1.7/containers/{0}/stop?'.format(container_id),
+            ('t', timeout))
         return (resp.code == 204)
 
     def kill_container(self, container_id):
@@ -214,7 +221,7 @@ class DockerHTTPClient(object):
         return (resp.code == 204)
 
     def get_image(self, name, size=4096):
-        parts = str(name).encode('utf-8').rsplit(':', 1)
+        parts = map(urllib.quote, str(name).encode('utf-8').rsplit(':', 1))
         url = '/v1.13/images/{0}/get'.format(parts[0])
         resp = self.make_request('GET', url)
 
@@ -226,14 +233,14 @@ class DockerHTTPClient(object):
         return
 
     def get_image_resp(self, name):
-        parts = str(name).encode('utf-8').rsplit(':', 1)
+        parts = urllib.quote(str(name).encode('utf-8').rsplit(':', 1))
         url = '/v1.13/images/{0}/get'.format(parts[0])
         resp = self.make_request('GET', url)
         return resp
 
     def load_repository(self, name, data):
         url = '/v1.13/images/load'
-        self.make_request('POST', url, data)
+        self.make_request('POST', url, data=data)
 
     def load_repository_file(self, name, path):
         with open(path) as fh:
@@ -241,18 +248,23 @@ class DockerHTTPClient(object):
 
     def commit_container(self, container_id, name):
         parts = str(name).encode('utf-8').rsplit(':', 1)
-        url = '/v1.7/commit?container={0}&repo={1}'.format(container_id,
-                                                           parts[0])
+        url = '/v1.7/commit?'
+        query = [('container', container_id),
+                 ('repo', parts[0])]
+
         if len(parts) > 1:
-            url += '&tag={0}'.format(parts[1])
-        resp = self.make_request('POST', url)
+            query += ('tag', parts[1])
+        resp = self.make_request('POST', url, *query)
         return (resp.code == 201)
 
     def get_container_logs(self, container_id):
         resp = self.make_request(
             'POST',
-            ('/v1.7/containers/{0}/attach'
-             '?logs=1&stream=0&stdout=1&stderr=1').format(container_id))
+            '/v1.7/containers/{0}/attach?'.format(container_id),
+            ('logs', '1'),
+            ('stream', '0'),
+            ('stdout', '1'),
+            ('stderr', '1'))
         if resp.code != 200:
             return
         return resp.read()

novadocker/tests/virt/docker/test_docker_client.py
@@ -44,13 +44,16 @@ class DockerHTTPClientTestCase(test.NoDBTestCase):
         headers = {'Content-Type': 'application/json'}
         kwargs['headers'] = headers
 
-        encoded_args = args[0], urllib.quote(args[1])
+        url = args[1]
+        if len(args) > 2:
+            url += urllib.urlencode(args[2:])
+        encoded_args = args[0], url
         mock_conn.request(*encoded_args, **kwargs)
         return mock_conn
 
     def test_list_containers(self):
         mock_conn = self.make_request('GET',
-                                      '/v1.7/containers/ps?all=1')
+                                      '/v1.7/containers/ps?', ('all', '1'))
         response = FakeResponse(200, data='[]',
                                 headers={'Content-Type': 'application/json'})
         mock_conn.getresponse().AndReturn(response)
@@ -88,9 +91,8 @@ class DockerHTTPClientTestCase(test.NoDBTestCase):
         })
 
         mock_conn = self.make_request('POST',
-                                      '/v1.7/containers/create?name={0}'.
-                                      format(
-                                      expected_uuid),
+                                      '/v1.7/containers/create?',
+                                      ('name', expected_uuid),
                                       body=expected_body)
         response = FakeResponse(201, data='{"id": "XXX"}',
                                 headers={'Content-Type': 'application/json'})
@@ -126,9 +128,8 @@ class DockerHTTPClientTestCase(test.NoDBTestCase):
             'VolumesFrom': '',
         })
         mock_conn = self.make_request('POST',
-                                      '/v1.7/containers/create?name={0}'.
-                                      format(
-                                      expected_uuid),
+                                      '/v1.7/containers/create?',
+                                      ('name', expected_uuid),
                                       body=expected_body)
         response = FakeResponse(201, data='{"id": "XXX"}',
                                 headers={'Content-Type': 'application/json'})
@@ -152,9 +153,8 @@ class DockerHTTPClientTestCase(test.NoDBTestCase):
         expected_uuid = uuid.uuid4()
 
         mock_conn = self.make_request('POST',
-                                      '/v1.7/containers/create?name={0}'.
-                                      format(
-                                      expected_uuid),
+                                      '/v1.7/containers/create?',
+                                      ('name', expected_uuid),
                                       body=mox.IgnoreArg())
         response = FakeResponse(201, data='{"ping": "pong"}',
                                 headers={'Content-Type': 'application/json'})
@@ -172,9 +172,8 @@ class DockerHTTPClientTestCase(test.NoDBTestCase):
         expected_uuid = uuid.uuid4()
 
         mock_conn = self.make_request('POST',
-                                      '/v1.7/containers/create?name={0}'
-                                      .format(
-                                      expected_uuid),
+                                      '/v1.7/containers/create?',
+                                      ('name', expected_uuid),
                                       body=mox.IgnoreArg())
         response = FakeResponse(400)
         mock_conn.getresponse().AndReturn(response)
@@ -270,7 +269,8 @@ class DockerHTTPClientTestCase(test.NoDBTestCase):
         self.mox.VerifyAll()
 
     def test_stop_container(self):
-        mock_conn = self.make_request('POST', '/v1.7/containers/XXX/stop?t=5')
+        mock_conn = self.make_request('POST', '/v1.7/containers/XXX/stop?',
+                                      ('t', '5'))
         response = FakeResponse(204,
                                 headers={'Content-Type': 'application/json'})
         mock_conn.getresponse().AndReturn(response)
@@ -296,7 +296,8 @@ class DockerHTTPClientTestCase(test.NoDBTestCase):
         self.mox.VerifyAll()
 
     def test_stop_container_bad_return_code(self):
-        mock_conn = self.make_request('POST', '/v1.7/containers/XXX/stop?t=5')
+        mock_conn = self.make_request('POST', '/v1.7/containers/XXX/stop?',
+                                      ('t', '5'))
         response = FakeResponse(400)
         mock_conn.getresponse().AndReturn(response)
 
@@ -346,7 +347,9 @@ class DockerHTTPClientTestCase(test.NoDBTestCase):
 
     def test_commit_container(self):
         mock_conn = self.make_request('POST',
-                                      '/v1.7/commit?container=XXX&repo=ping')
+                                      '/v1.7/commit?',
+                                      ('container', 'XXX'),
+                                      ('repo', 'ping'))
         response = FakeResponse(201,
                                 headers={'Content-Type': 'application/json'})
         mock_conn.getresponse().AndReturn(response)
@@ -360,7 +363,9 @@ class DockerHTTPClientTestCase(test.NoDBTestCase):
 
     def test_commit_container_bad_return_code(self):
         mock_conn = self.make_request('POST',
-                                      '/v1.7/commit?container=XXX&repo=ping')
+                                      '/v1.7/commit?',
+                                      ('container', 'XXX'),
+                                      ('repo', 'ping'))
         response = FakeResponse(400,
                                 headers={'Content-Type': 'application/json'})
         mock_conn.getresponse().AndReturn(response)
@@ -373,8 +378,12 @@ class DockerHTTPClientTestCase(test.NoDBTestCase):
         self.mox.VerifyAll()
 
     def test_get_container_logs(self):
-        url = '/v1.7/containers/XXX/attach?logs=1&stream=0&stdout=1&stderr=1'
-        mock_conn = self.make_request('POST', url)
+        url = '/v1.7/containers/XXX/attach?'
+        mock_conn = self.make_request('POST', url,
+                                      ('logs', '1'),
+                                      ('stream', '0'),
+                                      ('stdout', '1'),
+                                      ('stderr', '1'))
         response = FakeResponse(200, data='ping pong',
                                 headers={'Content-Type': 'application/json'})
         mock_conn.getresponse().AndReturn(response)
@@ -388,8 +397,12 @@ class DockerHTTPClientTestCase(test.NoDBTestCase):
         self.mox.VerifyAll()
 
     def test_get_container_logs_bad_return_code(self):
-        url = '/v1.7/containers/XXX/attach?logs=1&stream=0&stdout=1&stderr=1'
-        mock_conn = self.make_request('POST', url)
+        url = '/v1.7/containers/XXX/attach?'
+        mock_conn = self.make_request('POST', url,
+                                      ('logs', '1'),
+                                      ('stream', '0'),
+                                      ('stdout', '1'),
+                                      ('stderr', '1'))
         response = FakeResponse(404)
         mock_conn.getresponse().AndReturn(response)
 
@@ -426,7 +439,7 @@ class DockerHTTPClientTestCase(test.NoDBTestCase):
     def test_load_repository(self):
         data = ["hello", "world"]
         url = '/v1.13/images/load'
-        mock_conn = self.make_request('POST', url, data,
+        mock_conn = self.make_request('POST', url, data=data,
                                       headers={
                                       'Content-Type': 'application/json'})
         response = FakeResponse(200, data)