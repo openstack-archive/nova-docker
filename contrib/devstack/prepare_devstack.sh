e6c2760e7c6738ce495fe3310b3b133081ec5d36
Davanum Srinivas <dims@linux.vnet.ibm.com>
2014-10-15 19:43:27 -0400

    Port the docker driver to docker-py and remove custom client code
    For simplicity and ease of maintenance, remove the custom REST client
    code and imports docker-py.
    
    Co-Authored-By: Eric Windisch <ewindisch@docker.com>
    Change-Id: I3e17924b8319720ceb7cee480a81e953b809d0ee

--- contrib/devstack/prepare_devstack.sh
+++ contrib/devstack/prepare_devstack.sh
@@ -9,9 +9,12 @@ INSTALLDIR=${INSTALLDIR:-/opt/stack}
 cp $NOVADOCKERDIR/contrib/devstack/extras.d/70-docker.sh $INSTALLDIR/devstack/extras.d/
 cp $NOVADOCKERDIR/contrib/devstack/lib/nova_plugins/hypervisor-docker $INSTALLDIR/devstack/lib/nova_plugins/
 
+sudo PIP_DOWNLOAD_CACHE=/var/cache/pip http_proxy= https_proxy= no_proxy= /usr/bin/pip install docker-py
+
 cat - <<-EOF >> $INSTALLDIR/devstack/localrc
 export VIRT_DRIVER=docker
 export DEFAULT_IMAGE_NAME=cirros
+export NON_STANDARD_REQS=1
 export IMAGE_URLS=" "
 EOF
 
