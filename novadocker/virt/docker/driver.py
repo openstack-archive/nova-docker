361fce1175cd55096ef590c32bba5f69ff83dc63
Davanum Srinivas <dims@linux.vnet.ibm.com>
2014-11-23 15:26:10 -0500

    Ability to set the working directory
    Add a property working_directory for setting the WorkingDir
    parameter during container creation. Grab it from the
    instance metadata property.
    
    Change-Id: I226983e56763aa02ac970195f67ea83d213d58f1

--- novadocker/virt/docker/driver.py
+++ novadocker/virt/docker/driver.py
@@ -425,6 +425,11 @@ class DockerDriver(driver.ComputeDriver):
                 image_meta.get('properties', {}).get('os_command_line')):
             args['command'] = image_meta['properties'].get('os_command_line')
 
+        if (image_meta and
+                image_meta.get('properties', {}).get('working_directory')):
+            args['WorkingDir'] = image_meta['properties'].get(
+                'working_directory')
+
         container_id = self._create_container(instance, image_name, args)
         if not container_id:
             raise exception.InstanceDeployFailure(
