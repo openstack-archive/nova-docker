c6d6487baa09052396026adcca290228e6612ce7
Davanum Srinivas <dims@linux.vnet.ibm.com>
2015-01-05 21:42:33 -0500

    Add a new script for copying logs
    post_test_hook is not a good place to collect logs since it
    does not get executed when jobs fail. Let's try executing
    a script in our gate_hook to copy files over. Also use
    a find/exec to copy the files since some files may be gzipped
    
    Change-Id: I37b50eff7b086b3a29849a292a2171744460da06

--- contrib/devstack/copy_logs_hook.sh
+++ contrib/devstack/copy_logs_hook.sh
@@ -12,4 +12,16 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-# This script is executed inside post_test_hook function in devstack gate.
+# This script is executed inside gate_hook.sh
+
+INSTALLDIR=${INSTALLDIR:-/opt/stack}
+source $INSTALLDIR/devstack/functions-common
+
+if is_ubuntu; then
+  # Find and collect docker daemon logs
+  sudo find /var/log/ -name "docker*" -print -exec sudo cp {} /opt/stack/logs/ \;
+elif is_fedora; then
+  # fetch the docker logs from the journal
+  sudo journalctl _SYSTEMD_UNIT=docker.service | sudo tee -a $INSTALLDIR/logs/docker.service.log > /dev/null
+  sudo journalctl _SYSTEMD_UNIT=docker.socket | sudo tee -a $INSTALLDIR/logs/docker.socket.log > /dev/null
+fi
