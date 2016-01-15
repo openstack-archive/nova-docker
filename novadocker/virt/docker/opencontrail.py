984e149ba0a181d9843277e3679438b5f98278a4
Ignatious Johnson Christopher <ijohnson@juniper.net>
2016-01-15 10:39:17 -0800

    Using http based vrouter-port-control script to plug/unplug vif's in the vrouter. This script ensures that port info is persisted in file. Agent reads those files and re-adds port during restarts.
    Change-Id: Idc1cbb1cfbd70c9c7302031666a5c708d32425ac
    Closes-Bug: #1533037

--- novadocker/virt/docker/opencontrail.py
+++ novadocker/virt/docker/opencontrail.py
@@ -13,15 +13,10 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-import eventlet
-
-from contrail_vrouter_api.vrouter_api import ContrailVRouterApi
-
 from nova.network import linux_net
 from nova import utils
 
 from oslo_log import log as logging
-from oslo_service import loopingcall
 
 from novadocker.i18n import _
 
@@ -30,17 +25,63 @@ LOG = logging.getLogger(__name__)
 
 
 class OpenContrailVIFDriver(object):
-    def __init__(self):
-        self._vrouter_semaphore = eventlet.semaphore.Semaphore()
-        self._vrouter_client = ContrailVRouterApi(
-            doconnect=True, semaphore=self._vrouter_semaphore)
-        timer = loopingcall.FixedIntervalLoopingCall(self._keep_alive)
-        timer.start(interval=2)
+    def get_ipv4_network(self, vif):
+        ipv4_address = '0.0.0.0'
+        ipv4_netmask = '0.0.0.0'
+        ipv4_gateway = '0.0.0.0'
+        if 'subnets' in vif['network']:
+            subnets = vif['network']['subnets']
+            for subnet in subnets:
+                ips = subnet['ips'][0]
+                if (ips['version'] == 4):
+                    if ips['address'] is not None:
+                        ipv4_address = ips['address']
+                        ipv4_netmask = subnet['cidr'].split('/')[1]
+                        ipv4_gateway = subnet['gateway']['address']
+        return (ipv4_address, ipv4_netmask, ipv4_gateway)
 
-    def _keep_alive(self):
-        self._vrouter_client.periodic_connection_check()
+    def get_ipv6_network(self, vif):
+        ipv6_address = None
+        ipv6_netmask = None
+        ipv6_gateway = None
+        if 'subnets' in vif['network']:
+            subnets = vif['network']['subnets']
+            for subnet in subnets:
+                ips = subnet['ips'][0]
+                if (ips['version'] == 6):
+                    if ips['address'] is not None:
+                        ipv6_address = ips['address']
+                        ipv6_netmask = subnet['cidr'].split('/')[1]
+                        ipv6_gateway = subnet['gateway']['address']
+        return (ipv6_address, ipv6_netmask, ipv6_gateway)
+
+    def get_ifnames(self, vif):
+        if_local_name = 'veth%s' % vif['id'][:8]
+        if_remote_name = 'ns%s' % vif['id'][:8]
+        return (if_local_name, if_remote_name)
+
+    def add_port(self, instance, vif):
+        (if_local_name, _) = self.get_ifnames(vif)
+        (ipv4_address, _, _) = self.get_ipv4_network(vif)
+        (ipv6_address, _, _) = self.get_ipv6_network(vif)
+        ptype = 'NovaVMPort'
+
+        cmd_args = ("--oper=add --uuid=%s --instance_uuid=%s --vn_uuid=%s "
+                    "--vm_project_uuid=%s --ip_address=%s --ipv6_address=%s"
+                    " --vm_name=%s --mac=%s --tap_name=%s --port_type=%s "
+                    "--tx_vlan_id=%d --rx_vlan_id=%d" % (
+                        vif['id'], instance.uuid, vif['network']['id'],
+                        instance.project_id, ipv4_address, ipv6_address,
+                        instance.display_name, vif['address'],
+                        if_local_name, ptype, -1, -1))
+
+        utils.execute('vrouter-port-control', cmd_args, run_as_root=True)
 
     def plug(self, instance, vif):
+        """Plug into Contrail's network port
+
+        Bind the vif to a Contrail virtual port.
+        """
         vif_type = vif['type']
 
         LOG.debug('Plug vif_type=%(vif_type)s instance=%(instance)s '
@@ -48,8 +89,7 @@ class OpenContrailVIFDriver(object):
                   {'vif_type': vif_type, 'instance': instance,
                    'vif': vif})
 
-        if_local_name = 'veth%s' % vif['id'][:8]
-        if_remote_name = 'ns%s' % vif['id'][:8]
+        (if_local_name, if_remote_name) = self.get_ifnames(vif)
 
         # Device already exists so return.
         if linux_net.device_exists(if_local_name):
@@ -71,6 +111,10 @@ class OpenContrailVIFDriver(object):
             undo_mgr.rollback_and_reraise(msg=msg, instance=instance)
 
     def attach(self, instance, vif, container_id):
+        """Plug into Contrail's network port
+
+        Bind the vif to a Contrail virtual port.
+        """
         vif_type = vif['type']
 
         LOG.debug('Attach vif_type=%(vif_type)s instance=%(instance)s '
@@ -78,49 +122,16 @@ class OpenContrailVIFDriver(object):
                   {'vif_type': vif_type, 'instance': instance,
                    'vif': vif})
 
-        if_local_name = 'veth%s' % vif['id'][:8]
-        if_remote_name = 'ns%s' % vif['id'][:8]
+        (if_local_name, if_remote_name) = self.get_ifnames(vif)
 
         undo_mgr = utils.UndoManager()
         undo_mgr.undo_with(lambda: utils.execute(
             'ip', 'link', 'delete', if_local_name, run_as_root=True))
-        ipv4_address = '0.0.0.0'
-        ipv6_address = None
-        if 'subnets' in vif['network']:
-            subnets = vif['network']['subnets']
-            for subnet in subnets:
-                ips = subnet['ips'][0]
-                if (ips['version'] == 4):
-                    if ips['address'] is not None:
-                        ipv4_address = ips['address']
-                        ipv4_netmask = subnet['cidr'].split('/')[1]
-                        ipv4_gateway = subnet['gateway']['address']
-                if (ips['version'] == 6):
-                    if ips['address'] is not None:
-                        ipv6_address = ips['address']
-                        ipv6_netmask = subnet['cidr'].split('/')[1]
-                        ipv6_gateway = subnet['gateway']['address']
-        params = {
-            'ip_address': ipv4_address,
-            'vn_id': vif['network']['id'],
-            'display_name': instance['display_name'],
-            'hostname': instance['hostname'],
-            'host': instance['host'],
-            'vm_project_id': instance['project_id'],
-            'port_type': 'NovaVMPort',
-            'ip6_address': ipv6_address,
-        }
-
         try:
             utils.execute('ip', 'link', 'set', if_remote_name, 'netns',
                           container_id, run_as_root=True)
 
-            result = self._vrouter_client.add_port(
-                instance['uuid'], vif['id'],
-                if_local_name, vif['address'], **params)
-            if not result:
-                # follow the exception path
-                raise RuntimeError('add_port returned %s' % str(result))
+            self.add_port(instance, vif)
             utils.execute('ip', 'link', 'set', if_local_name, 'up',
                           run_as_root=True)
         except Exception:
@@ -132,6 +143,9 @@ class OpenContrailVIFDriver(object):
             utils.execute('ip', 'netns', 'exec', container_id, 'ip', 'link',
                           'set', if_remote_name, 'address', vif['address'],
                           run_as_root=True)
+            (ipv6_address,
+             ipv6_netmask,
+             ipv6_gateway) = self.get_ipv6_network(vif)
             if ipv6_address:
                 ip = ipv6_address + "/" + ipv6_netmask
                 gateway = ipv6_gateway
@@ -141,6 +155,9 @@ class OpenContrailVIFDriver(object):
                 utils.execute('ip', 'netns', 'exec', container_id, 'ip', '-6',
                               'route', 'replace', 'default', 'via', gateway,
                               'dev', if_remote_name, run_as_root=True)
+            (ipv4_address,
+             ipv4_netmask,
+             ipv4_gateway) = self.get_ipv4_network(vif)
             if ipv4_address != '0.0.0.0':
                 ip = ipv4_address + "/" + ipv4_netmask
                 gateway = ipv4_gateway
@@ -155,6 +172,10 @@ class OpenContrailVIFDriver(object):
             LOG.exception(_("Failed to attach vif"), instance=instance)
 
     def unplug(self, instance, vif):
+        """Unplug Contrail's network port
+
+        Unbind the vif from a Contrail virtual port.
+        """
         vif_type = vif['type']
         if_local_name = 'veth%s' % vif['id'][:8]
 
@@ -163,8 +184,9 @@ class OpenContrailVIFDriver(object):
                   {'vif_type': vif_type, 'instance': instance,
                    'vif': vif})
 
+        cmd_args = ("--oper=delete --uuid=%s" % (vif['id']))
         try:
-            self._vrouter_client.delete_port(vif['id'])
+            utils.execute('vrouter-port-control', cmd_args, run_as_root=True)
             if linux_net.device_exists(if_local_name):
                 utils.execute('ip', 'link', 'delete', if_local_name,
                               run_as_root=True)
