fde1c67d4d3f8bd745a67934129bb08484bf758b
Davanum Srinivas <davanum@gmail.com>
2016-01-26 11:05:22 -0600

    [WIP] Switch from hack to a DevStack plugin
    Change-Id: Ib5c6751b10fc3fb8fef2f3647c514f6d833a5597

--- contrib/devstack/prepare_devstack.sh
+++ contrib/devstack/prepare_devstack.sh
@@ -6,13 +6,12 @@ env
 NOVADOCKERDIR=$(readlink -f $(dirname $0)/../..)
 INSTALLDIR=${INSTALLDIR:-/opt/stack}
 
-cp $NOVADOCKERDIR/contrib/devstack/extras.d/70-docker.sh $INSTALLDIR/devstack/extras.d/
 cp $NOVADOCKERDIR/contrib/devstack/lib/nova_plugins/hypervisor-docker $INSTALLDIR/devstack/lib/nova_plugins/
 
 cat - <<-EOF >> $INSTALLDIR/devstack/localrc
-export VIRT_DRIVER=docker
-export DEFAULT_IMAGE_NAME=cirros
-export NON_STANDARD_REQS=1
-export IMAGE_URLS=" "
+RECLONE=True
+git_clone https://github.com/dims/devstack-plugin-docker /opt/stack/docker-service master
+RECLONE=False
+enable_plugin docker-service https://github.com/dims/devstack-plugin-docker
 EOF
 
