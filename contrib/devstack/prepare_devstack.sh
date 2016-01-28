1f09d43b63c7672106b87f59d847e5104b4624c5
Davanum Srinivas <davanum@gmail.com>
2016-01-28 16:40:16 -0500

    Convert old code in extras.d into devstack plugin
    extras.d is no longer supported, we need a devstack plugin that
    can be used for installing docker service
    
    Change-Id: I0f68cd8819f01398a3d7421f7a06a9540349391b

--- contrib/devstack/prepare_devstack.sh
+++ contrib/devstack/prepare_devstack.sh
@@ -9,9 +9,6 @@ INSTALLDIR=${INSTALLDIR:-/opt/stack}
 cp $NOVADOCKERDIR/contrib/devstack/lib/nova_plugins/hypervisor-docker $INSTALLDIR/devstack/lib/nova_plugins/
 
 cat - <<-EOF >> $INSTALLDIR/devstack/localrc
-RECLONE=True
-git_clone https://github.com/dims/devstack-plugin-docker /opt/stack/docker-service master
-RECLONE=False
-enable_plugin docker-service https://github.com/dims/devstack-plugin-docker
+enable_plugin nova-docker https://git.openstack.org/openstack/nova-docker
+GITDIR['nova-docker']=$INSTALLDIR/nova-docker
 EOF
-
