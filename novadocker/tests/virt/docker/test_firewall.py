97ae25eb8f2dfd27d97a2e6572e987e07d596b8b
sayalilunkad <sayali.lunkad@suse.com>
2016-12-07 15:34:56 +0100

    Shim compute driver in nova.virt namespace
    Similar to what is done for nova-powervm due to the change in nova
    which removes the mechanism to load ComputeDrivers out of the tree.
    nova commit 8eb03de1eb83a6cd2d4d41804e1b8253f94e5400
    nova-powervm commit fe9bec248e267d6c714a8ff9ea1a025381298fd9
    
    Change-Id: I1eab0a0c4e7a2c37fa8021705afd30744335e294
    Partial-Bug: 1575335

--- novadocker/tests/virt/docker/test_firewall.py
+++ novadocker/tests/virt/docker/test_firewall.py
@@ -18,7 +18,7 @@ import mock
 from nova.network import model as network_model
 from nova import test
 from nova.virt import firewall
-from novadocker.virt.docker import driver
+from nova.virt.docker import driver
 
 
 class DockerFirewallDriverTestCase(test.TestCase):
